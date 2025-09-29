e10e4fcdbf2ff2176f414009de29b84b11e33e8b
jose-ant-1 <jgonsol1510@g.educaand.es>
2025-09-29 21:36:25 +0200

    arreglo para que funcione con el script en cualquier lugar

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 console.log('Happy developing ✨')
 
 let frutas = ["platano", "manzana", "piña", "aguacate"];
-const mosrtar = document.getElementById("mosrtar");
-let frutaABuscar = prompt("que posición de que fruta quieres mostrar?").toLowerCase();
-
+let frutaABuscar;
 let posicion = frutas.indexOf(frutaABuscar);
 
-if (posicion !== -1) {
-    mostrar.textContent = "La fruta " + frutaABuscar + " está en la posición " + posicion + "del array";
-} else {
-    mostrar.textContent = "La fruta " + frutaABuscar + " no se encontró en la lista";
-}
 console.log(frutas);
-//hacer con forEach junto con su metodo de array
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+    const mostrar = document.getElementById("mosrtar");
+
+    frutaABuscar = prompt("que posición de que fruta quieres mostrar?").toLowerCase();
+
+    if (posicion !== -1) {
+        mostrar.textContent = "La fruta " + frutaABuscar + " está en la posición " + posicion + "del array";
+    } else {
+        mostrar.textContent = "La fruta " + frutaABuscar + " no se encontró en la lista";
+    }
+
+    frutas.forEach((fruta) => console.log(fruta));
+
+});
+
+/* hacer con forEach junto con su metodo de array
 for (i = 0; i < frutas.length; i++) {
     console.log(frutas[i]);
 }
-
-
+ */
